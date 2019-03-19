d766ee59659c7be2669dc1ed360a35c05ee698db
Tim Beck <tim.beck46@gmail.com>
2019-03-19 13:38:43 -0400

    Wire up parser and get test for add to pass

--- parser.rb
+++ parser.rb
@@ -1,6 +1,34 @@
-def Parser
+require 'pry'
+
+class Parser
+  attr_reader :command, :args
+
   def initialize(str)
-    @buffer = StringScanner.new(str)
+    @buffer = StringScanner.new str
+    @command = ''
+    @args = []
     parse
+  end
+
+  def parse
+    until @buffer.eos?
+      parse_input
+    end
+  end
+
+  def parse_input
+    @command = @buffer.string unless @buffer.exist? /"/
+
+    command = @buffer.scan_until /"/
+    @command = command.delete_suffix(' "')
+
+    @buffer.pos = @buffer.pos - 1
+
+    args_with_quotes = []
+    until @buffer.eos?
+      args_with_quotes << @buffer.scan(/"([^"]*)"/)
+      @buffer.skip(/\s+/)
+      @args = args_with_quotes.map { |arg| arg.delete_prefix('"').delete_suffix('"') }
+    end
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,3 +1,13 @@
 require_relative '../parser'
 
-let(:parser) { Parser.new 'add "ride the lightning" "metallica"' }
+describe Parser do
+  let(:parser) { Parser.new 'add "ride the lightning" "metallica"' }
+
+  it 'correctly finds the command from a string' do
+    expect(parser.command).to eql('add')
+  end
+
+  it 'correctly finds the arguments from the string' do
+    expect(parser.args).to include('ride the lightning', 'metallica')
+  end
+end

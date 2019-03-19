de7c320a6bcb976cba7a3c5ffc2ee3904ff2ccf4
Tim Beck <tim.beck46@gmail.com>
2019-03-19 13:44:07 -0400

    Wrap add spec in context block

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,13 +1,15 @@
 require_relative '../parser'
 
 describe Parser do
-  let(:parser) { Parser.new 'add "ride the lightning" "metallica"' }
+  context 'Add command' do
+    let(:parser) { Parser.new 'add "ride the lightning" "metallica"' }
 
-  it 'correctly finds the command from a string' do
-    expect(parser.command).to eql('add')
-  end
+    it 'correctly finds the command from a string' do
+      expect(parser.command).to eql('add')
+    end
 
-  it 'correctly finds the arguments from the string' do
-    expect(parser.args).to include('ride the lightning', 'metallica')
+    it 'correctly finds the arguments from the string' do
+      expect(parser.args).to include('ride the lightning', 'metallica')
+    end
   end
 end
